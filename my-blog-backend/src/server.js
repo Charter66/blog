d75a65365d6b7da92b5f37f7b9620f15d8b16206
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-07-21 19:30:15 +0200

    not workin, trying

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 const credentials = JSON.parse(
-    fs.readFileSync('../credentials.json')
+    fs.readFileSync(__dirname,'..','credentials.json')
     );
     admin.initializeApp({
         credential : admin.credential.cert(credentials),
