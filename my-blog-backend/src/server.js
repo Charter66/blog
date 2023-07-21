4770bb44ffa4653378ecbc423013e89165dab27c
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-07-21 20:14:44 +0200

    deleted build indexhtml

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get(/^(?!\/api).+/,(req,res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+// app.get(/^(?!\/api).+/,(req,res) => {
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
 
 
 app.use(async(req,res, next )=> {
