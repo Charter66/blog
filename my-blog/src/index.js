954dce635c64da93cd89118c616538d3112daa8f
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-07-24 21:37:45 +0200

    removed the app

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
