649d8b2620b8a7e4b85644b8e8d0a8882aad5020
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-07-24 20:14:29 +0200

    oasjj

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../public/index.html')));
 
 // app.get(/^(?!\/api).+/,(req,res) => {
 //     res.sendFile(path.join(__dirname, '../build/index.html'));
