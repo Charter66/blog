a9eda950d6683959741e7fd3342e3b5086691a23
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-07-24 20:53:52 +0200

    saoi;jd

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 // Import the functions you need from the SDKs you need
