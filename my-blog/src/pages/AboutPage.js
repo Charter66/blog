368db114594ec80a314acb72df8bbd295f4aec80
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-08-22 11:36:41 +0200

    create

--- my-blog/src/pages/AboutPage.js
+++ my-blog/src/pages/AboutPage.js
@@ -21,7 +21,7 @@ const AboutPage = () => {
         <p>
             Etiam neceeeee lec btus urna. Sed sodales ultrices dapibus. 
             Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            nisi. Nunc vel pulvinasadasr diam. Nam eleifend egestas viverra. Donec finibus lectus 
             sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
             consequat quam. Vivamus accumsan dui in facilisis aliquet.,
