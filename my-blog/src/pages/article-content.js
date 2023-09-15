fe93f035963d0c627dc5cd02ee34fd3be6faf3ea
Lazar Godjirov <lazargodzirov@yahoo.com>
2023-09-15 17:29:20 +0200

    sapodk

--- my-blog/src/pages/article-content.js
+++ my-blog/src/pages/article-content.js
@@ -16,7 +16,7 @@ const articles = [
             sed lorem ultricies, eget ornare leo lucterus. Morbi vehicula, nulla eu tempor 
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
             consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            `Etiam nec lectus urasdana. Sed sodales ultrices dapibus. 
             Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
             nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
             sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
